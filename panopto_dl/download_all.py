d12f3a02d74f6a461cb79bd96408e99c2563e0be
Samson Qian <samsonqian@gmail.com>
2023-08-13 21:59:23 -0400

    download all script

--- panopto_dl/download_all.py
+++ panopto_dl/download_all.py
@@ -1,25 +1,36 @@
-import PanoptoDownloader
+import os
+import sys
+from tqdm import tqdm
+
+from panopto_dl.PanoptoDownloader import PanoptoDownloader
 
 
-URL = input('URL: ')
-PATH = input('Output file: ') or './output.mp4'
-
+# save to local
+LOCAL_FOLDER = "/Users/samsonqian/Desktop/MIT Panopto"
 
 def callback(progress: int):
     """
     :param progress: Downloading progress. From 0 to 100
     """
-    print(f"{progress} / 100")
+    #print(f"{progress} / 100")
+    return
 
 
-if __name__ == '__main__':
-    try:
-        PanoptoDownloader.download(
-            URL,
-            PATH,
-            callback
-        )
-        print("Download completed")
+if __name__ == "__main__":
+    counter = 1
+    with open("test.txt", "r") as f:
+        lines = f.readlines()
+        total_lines = len(lines)
+        for url in tqdm(lines, total=total_lines, position=0):
+            try:
+                url = url.strip()
+                PATH = os.path.join(LOCAL_FOLDER, url.split("/")[-1])
+                PanoptoDownloader.download(url,
+                                           PATH,
+                                           callback)
 
-    except Exception as e:
-        print(e)
+            except Exception as e:
+                print(e)
+
+            print("Completed: {}/{}".format(counter, total_lines))
+            counter += 1
